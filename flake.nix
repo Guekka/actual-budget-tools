9f1be9a7eb8e7a997affaad54af3994799f32a1c
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-05-30 08:22:06 +0100

    build: setup nix dev env

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    formatter.x86_64-linux = pkgs.alejandra;
+
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; let
+        pandasql = pkgs.python3Packages.buildPythonPackage rec {
+          pname = "pandasql";
+          version = "0.7.3";
+
+          src = pkgs.python3Packages.fetchPypi {
+            inherit version;
+            inherit pname;
+            sha256 = "sha256-HrJIhpCGQ1p9hSgevZ/lJdadnZVKDc64VPcajQ/Y3mk=";
+          };
+
+          buildInputs = with python3Packages; [sqlalchemy pandas];
+          doCheck = false;
+        };
+        currency-converter = pkgs.python3Packages.buildPythonPackage rec {
+          pname = "currency-converter";
+          version = "0.17.9";
+
+          src = pkgs.python3Packages.fetchPypi {
+            inherit version;
+            pname = "CurrencyConverter";
+            sha256 = "sha256-P4mu0VJRgc9XHSSwJ85opWd1d8UaJunKpqyL60MwJpY=";
+          };
+
+          buildInputs = with python3Packages; [setuptools-scm];
+
+          doCheck = false;
+        };
+      in [
+        (python3.withPackages (ps:
+          with ps;
+          with python3Packages; [
+            pandas
+            pandasql
+            python-dotenv
+            pygments
+            pyperclip
+            requests
+            plotly
+            tabulate
+            numpy
+            pre-commit-hooks
+            sqlalchemy
+            prompt-toolkit
+            currency-converter
+            yapf
+            jupyter-packaging
+          ]))
+
+        pre-commit
+      ];
+    };
+  };
+}
